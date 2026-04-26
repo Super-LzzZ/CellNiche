ffd75a046685a39f8d3ac35a75c0e799ae1bda2c
Zhongming Liang <lzzzmmgpt@gmail.com>
2026-04-26 20:55:08 +0800

    Release v0.1.8 with multi-slice and embedding support

--- cellniche/__init__.py
+++ cellniche/__init__.py
@@ -2,7 +2,7 @@
 Author: Zhongming Liang lzzzmmgpt@gmail.com
 Date: 2026-04-26 15:22:43
 LastEditors: Zhongming Liang lzzzmmgpt@gmail.com
-LastEditTime: 2026-04-26 15:22:44
+LastEditTime: 2026-04-26 20:52:04
 FilePath: /liangzhongming/phd_code/ModelTest/CellNiche/cellniche/__init__.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -10,7 +10,7 @@ from importlib import import_module
 from types import ModuleType
 from typing import Dict, Any
 
-__version__ = "0.1.6"
+__version__ = "0.1.8"
 __author__  = "ZMLiang <lzzzmmgpt@gmail.com>"
 
 _EXPORTS = {
